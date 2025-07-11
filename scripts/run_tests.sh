76ec1bb8c60c8c0a17530a12c1dc27d1b7b69b32
paolodelia99 <paolo.delia99@gmail.com>
2025-07-11 12:19:07 +0200

    compiling proto files before tests execution

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,5 +8,8 @@ pip install pytest
 # Test suites to run
 tests=("tests/black_scholes" "tests/datetime" "tests/utils" "tests/experimental")
 
+echo "Compiling proto files!"
+protoc --python_out=. --proto_path=. tf_quant_finance/experimental/pricing_platform/instrument_protos/*.proto
+
 echo "Running tff tests"
 pytest --no-header -vv "${tests[@]}"
